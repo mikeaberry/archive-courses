3f6047d00faa07f309563a76f9becf771979811e
mikeaberry <mikeyaberry@gmail.com>
2024-10-09 10:07:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-let url_path = document.querySelectorAll("a");
-
-const aTags = document.querySelectorAll('a') 
-aTags.forEach(aTag => {
-//   aTag.href = "NewHrefAttribute"
-  console.log("url list",aTag.href);
- let url_list = document.getElementById('list');
- let pattern = '/\n/'
-
- url_list.innerHTML+= aTag.href+'\r';
-
-});
+// This file has been moved to 2023-2024 folder.
